7d254189eaa5f8438ed6d61c962bd6066d2cc558
wangy1b <wangy1b@163.com>
2021-05-09 01:08:04 +0800

    测试shardingjdbc分库分表

--- src/test/java/com/wyb/shardingjdbc/ShardingJdbcApplicationTests.java
+++ src/test/java/com/wyb/shardingjdbc/ShardingJdbcApplicationTests.java
@@ -1,15 +1,36 @@
 package com.wyb.shardingjdbc;
 
+import com.wyb.shardingjdbc.entity.User;
+import com.wyb.shardingjdbc.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
 @SpringBootTest
 class ShardingJdbcApplicationTests {
 
+	@Autowired
+	private UserMapper userMapper;
 	@Test
-	void contextLoads() {
+	void contextLoads() throws ParseException {
+		User user = new User();
+		user.setNickname("user_" + new Random().nextInt(100));
+		user.setPassword("12345" );
+		user.setAge(new Random().nextInt(80));
+		user.setSex(new Random().nextInt(1));
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");//注意月份是MM
+		// Date date = simpleDateFormat.parse("1999-05-09");
+		Date date = simpleDateFormat.parse("2020-05-09");
+		user.setBirthday(date);
+		userMapper.addUser(user);
+
 	}
 
 }
