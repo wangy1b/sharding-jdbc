0134081699442ddf87bc3ed3404552d9b8259f9a
wangy1b <wangy1b@163.com>
2021-05-08 18:56:44 +0800

    测试读写分离

--- src/main/java/com/wyb/shardingjdbc/controller/UserController.java
+++ src/main/java/com/wyb/shardingjdbc/controller/UserController.java
@@ -4,25 +4,36 @@ import com.wyb.shardingjdbc.entity.User;
 import com.wyb.shardingjdbc.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
-public class UserController {
+@RestController
+@RequestMapping("/user")
+public class UserController  {
     @Autowired
     private UserMapper userMapper;
-    @GetMapping("/slave")
-    public String insert() {
+
+    @GetMapping("/save")
+    public String insert() throws ParseException {
         User user = new User();
-        user.setNickname("zhangsan" + new Random().nextInt());
+        user.setNickname("user_" + new Random().nextInt());
         user.setPassword("12345" );
         user.setAge(new Random().nextInt(80));
         user.setSex(new Random().nextInt(1));
-        user.setBirthday("1988-12-03");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");//注意月份是MM
+        Date date = simpleDateFormat.parse("1988-12-03");
+        user.setBirthday(date);
         userMapper.addUser(user);
         return "success";
     }
-    @GetMapping("listuser")
+    @GetMapping("/listuser")
     public List<User> listuser() {
         return userMapper.findUsers();
     }
--- src/main/java/com/wyb/shardingjdbc/mapper/UserMapper.java
+++ src/main/java/com/wyb/shardingjdbc/mapper/UserMapper.java
@@ -2,10 +2,14 @@ package com.wyb.shardingjdbc.mapper;
 
 import com.wyb.shardingjdbc.entity.User;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Mapper
 public interface UserMapper {
     @Insert("insert into ksd_user(nickname,password,age,sex,birthday) values(#{nickname},#{password},#{age},#{sex},#{birthday})")
     void addUser(User user);
